0ef0829ee98ea04540a0b8c4f0f5ffd622c5c322
Michael Howell <michael@notriddle.com>
2022-10-28 09:51:22 -0700

    search-container

---
+++ search-container/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = JSON.parse('{\
+"test_dingus":{"doc":"","t":[5],"n":["test"],"q":["test_dingus"],"d":["Examples"],"i":[0],"f":[[[]]],"p":[]}\
+}');
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
