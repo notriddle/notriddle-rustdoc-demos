b9014c8d1b43b7a2ca7ac7d00d1413d71014ade0
Michael Howell <michael@notriddle.com>
2022-12-13 11:13:48 -0700

    =stab-baseline

---
+++ stab-baseline/implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"test_dingus":[["impl Freeze for <a class=\"struct\" href=\"test_dingus/struct.Test.html\" title=\"struct test_dingus::Test\">Test</a>",1,["test_dingus::Test"]]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
