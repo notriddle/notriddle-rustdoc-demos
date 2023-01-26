ba664a210d6aac0129e82efddb26333a449642a2
Michael Howell <michael@notriddle.com>
2023-01-26 14:48:18 -0700

    simplify-doctest-tooltip

---
+++ simplify-doctest-tooltip/std/os/linux/sidebar-items1.69.0.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":[["fs","Linux-specific extensions to primitives in the `std::fs` module."],["net","Linux-specific networking functionality."],["process","Linux-specific extensions to primitives in the `std::process` module."],["raw","Linux-specific raw type definitions."]]};
